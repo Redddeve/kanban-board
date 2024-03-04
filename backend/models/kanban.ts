14c3c8444b553d0343a9a2e9129c97bd7c02da52
Redddeve <popko.dmitry797@gmail.com>
2024-03-04 10:32:23 +0200

    Remove backend from repo

--- backend/models/kanban.ts
+++
@@ -1,8 +0,0 @@
-import { model } from 'mongoose';
-import { boardSchema, cardSchema } from '../schemas/mongoose/kanban';
-
-const Board = model('Board', boardSchema);
-
-const Card = model('Card', cardSchema);
-
-export { Board, Card };
